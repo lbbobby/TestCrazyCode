db280863d2d0eb426a81c4d6ebae17b721f21b93
lbbobby <785636688@qq.com>
2015-09-06 10:55:34 +0800

    新建工程

---
+++ Assets/code/ClientMain.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClientMain : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/code/ConfigMgr.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfigMgr : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}

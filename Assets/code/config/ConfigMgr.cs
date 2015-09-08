36a336bbd5d67bc9a30dfb0997e013a1eea2e7e6
lbbobby <785636688@qq.com>
2015-09-08 22:57:54 +0800

    添加配置

---
+++ Assets/code/config/ConfigMgr.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BaseConfig
+{
+    public virtual void Parse() 
+    {
+        
+    }
+
+    public virtual void TestWrite()
+    { 
+        
+    }
+}
+
+public class ConfigMgr
+{
+
+
+}
